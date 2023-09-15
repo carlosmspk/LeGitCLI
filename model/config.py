a3f0f5f665239ad58a5dbf6fe371be4c7140c024
carlosmspk <carlos_cp_santos@hotmail.com>
2023-09-15 13:40:50 +0100

    LG4 Add docstrings to Config class

--- model/config.py
+++ model/config.py
@@ -3,4 +3,6 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Config:
+    """Top level configurations of how LeGit should behave, regardless of matched scopes and rules"""
+
     ...  # TODO
