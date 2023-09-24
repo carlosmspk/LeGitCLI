d412e802d5660cd24b58fbfedb7694775b0c6d77
carlosmspk <carlos_cp_santos@hotmail.com>
2023-09-24 23:06:46 +0100

    #10 Add GitClient class for git operations

--- git/client.py
+++ git/client.py
@@ -1,3 +1,4 @@
+from os import PathLike
 from model.command_configs import ErrorBehavior, OutputLevel
 
 
@@ -23,5 +24,41 @@ class GitReadonlyClient:
         """
         Returns the configured user/author. Result is returned as tuple of name
         and email, respectively
+        """
+        ...
+
+
+class GitClient(GitReadonlyClient):
+    """
+    Client class that acts upon a git repository found in the process's current
+    directory.
+    """
+
+    def init(self, main_branch: str):
+        """Initializes a git repository at current directory. Accepts an initial
+        main branch name (default: 'main')"""
+        ...
+
+    def commit(self, message: str):
+        """
+        Commits staged changes with `message`.
+
+        Raises an exception if commit fails, e.g. because there are no staged
+        files or message is invalid.
+
+        Mutating method: calling this method will raise an exception if this
+        client instance is set as readonly
+        """
+        ...
+
+    def stage(self, file_path: str):
+        """
+        Stages `file_path` for committing.
+
+        Raises an exception if stage fails, e.g. because file does not exist or
+        has no changes.
+
+        Mutating method: calling this method will raise an exception if this
+        client instance is set as readonly
         """
         ...
