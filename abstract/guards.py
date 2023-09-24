6fe233f96de3ab4096fae97d8be518df8fa486cb
carlosmspk <carlos_cp_santos@hotmail.com>
2023-09-24 22:55:28 +0100

    #5 Fix import of renamed AbstractClassInstantiatedError

--- abstract/guards.py
+++ abstract/guards.py
@@ -1,7 +1,7 @@
 from typing import Any
-from model.exceptions import IllegalAbstractClassInitCall
+from model.exceptions import AbstractClassInstantiatedError
 
 
 def prevent_abstract_instantiation(self: Any, abstract_type: type):
     if self.__class__ == abstract_type:
-        raise IllegalAbstractClassInitCall(self.__class__)
+        raise AbstractClassInstantiatedError(self.__class__)
