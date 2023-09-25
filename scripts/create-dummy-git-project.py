aac3472af8377a87fb99722a5bb0dd7b9590857c
carlosmspk <carlos_cp_santos@hotmail.com>
2023-09-25 21:50:17 +0100

    #11 Add script to create subproject for testing under sample_projects folder

---
+++ scripts/create-dummy-git-project.py
@@ -0,0 +1,69 @@
+if __name__ == "__main__":
+    import argparse
+    import shutil
+    import os
+    from scriptutils import (
+        get_valid_command_from,
+        run_command,
+        set_project_root_as_active_dir,
+    )
+    import sys
+
+    project_root_dir = set_project_root_as_active_dir()
+
+    main_py_dir = os.path.join(project_root_dir, "main.py")
+    if not os.path.exists(main_py_dir):
+        print(
+            f"Could not find main.py file to reference. Perhaps either main.py or this script were moved? Expected main.py file to be at: {main_py_dir}"
+        )
+        exit(1)
+
+    parser = argparse.ArgumentParser(
+        description="Creates a subfolder at project's root dir with name TargetProject or whatever name is given by the --target argument, if given. This folder will then be setup to be a git repo with a single commit, and have a content.txt file"
+    )
+    parser.add_argument("--target", type=str, default="default")
+    target: str = os.path.join(
+        project_root_dir, "sample_projects", parser.parse_args().target
+    )
+
+    python_command = sys.executable
+
+    if get_valid_command_from("git") is None:
+        print("Could not find git")
+        exit(1)
+
+    if os.path.exists(target):
+        invalid_input = True
+        while invalid_input:
+            user_input = input(
+                f"Folder/file '{target}' already exists. Do you wish to remove it? [Y/n]"
+            )
+            if user_input.lower() in ("y", "yes"):
+                shutil.rmtree(target)
+                invalid_input = False
+            elif user_input.lower() in ("n", "no"):
+                print("Aborted.")
+                exit(1)
+            else:
+                print(
+                    f"Unrecognized input: '{user_input}'. Please provide one of 'n' or 'y'"
+                )
+
+    os.makedirs(target)
+    with open(os.path.join(target, "content.txt"), "w") as f:
+        f.write("EDIT AND COMMIT ME\n")
+
+    os.chdir(target)
+    run_command("git init >/dev/null")
+    run_command('git config --local user.name "user" >/dev/null')
+    run_command('git config --local user.email "user@mail.com" >/dev/null')
+    run_command("git add . >/dev/null")
+    run_command('git commit -m "setup" >/dev/null')
+
+    pre_commit_hook_file = os.path.join(".git", "hooks", "pre-commit")
+
+    with open(os.path.join(target, pre_commit_hook_file), "w") as f:
+        f.write(f"{python_command} {main_py_dir}\n")
+    os.chmod(pre_commit_hook_file, 0o777)
+
+    print(f"Created new git project at ./{os.path.relpath(target, project_root_dir)}")
