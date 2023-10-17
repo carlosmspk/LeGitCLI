fe405167b201f1dbaf947317f1640e68c477dfda
carlosmspk <carlos_cp_santos@hotmail.com>
2023-10-17 15:40:17 +0100

    #16 Add LegitRulesMissingFieldError exception

--- model/exceptions.py
+++ model/exceptions.py
@@ -1,7 +1,19 @@
+from typing import Iterable
+
+
 class AbstractClassInstantiatedError(Exception):
     """Attempted to instantiate an abstract class"""
 
     def __init__(self, abstract_class_type: type) -> None:
         super().__init__(
             f"Cannot instantiate Abstract class '{abstract_class_type.__name__}' directly."
+        )
+
+
+class LeGitRulesMissingYamlFieldError(Exception):
+    """Parsed LeGit file does not have expected field"""
+
+    def __init__(self, required_field_name: str, field_path: Iterable[str]) -> None:
+        super().__init__(
+            f"Missing required field '{required_field_name}' at '{' > '.join(field_path)} > {required_field_name}'"
         )
