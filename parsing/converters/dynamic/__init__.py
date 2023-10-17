afcf0da6f5c66e64eddce2c8351b693ff61d538f
carlosmspk <carlos_cp_santos@hotmail.com>
2023-10-17 15:40:21 +0100

    #16 Add dynamic scope condition converter class ConcreteScopeConditionParametersConverter

--- parsing/converters/dynamic/__init__.py
+++ parsing/converters/dynamic/__init__.py
@@ -1,11 +1,14 @@
 from dataclasses import fields
-from model.exceptions import UnknownLegitEntityTypeError
+from model.exceptions import (
+    UnknownLegitEntityTypeError,
+)
 from model.rules import Rule
-from model.bindings import rule_bindings_map
+from model.bindings import rule_bindings_map, scope_bindings_map
+from model.scope_conditions import ScopeCondition, ScopeConditionAction
 from parsing.converters.base_converter import BaseConverter
 
 
-def _snake_to_pascal(s: str) -> str:
+def _snake_to_pascal(s):
     return "".join(part.capitalize() for part in s.split("_"))
 
 
@@ -15,6 +18,7 @@ class ConcreteRuleParametersConverter(BaseConverter[dict, Rule]):
     ) -> None:
         super().__init__(object_to_convert, field_path, dict)
         if rule_type not in rule_bindings_map:
+            print([i for i in rule_bindings_map])
             raise UnknownLegitEntityTypeError(
                 entity="Rule", unknown_entity_name=rule_type
             )
@@ -33,3 +37,43 @@ class ConcreteRuleParametersConverter(BaseConverter[dict, Rule]):
             for param_python_name, param_yaml_name in field_names.items()
         }
         return self.RuleDataclass(**field_values)
+
+
+class ConcreteScopeConditionParametersConverter(BaseConverter[dict, ScopeCondition]):
+    def __init__(
+        self,
+        object_to_convert: dict,
+        field_path: list[str],
+        scope_action: str,
+        scope_type: str,
+    ) -> None:
+        super().__init__(object_to_convert, field_path, dict)
+        if scope_type not in scope_bindings_map:
+            raise UnknownLegitEntityTypeError(
+                entity="Scope Condition", unknown_entity_name=scope_type
+            )
+        self.ScopeConditionDataclass = scope_bindings_map[scope_type]
+        self.scope_action = self._get_action_enum_from_string(scope_action)
+
+    def convert(self) -> ScopeCondition:
+        field_values: dict[str, str] = {"action": self.scope_action}
+
+        field_names = {
+            field.name: _snake_to_pascal(field.name)
+            for field in fields(self.ScopeConditionDataclass)
+            if field.name != "action"
+        }
+
+        self._assert_fields(required_field_names=set(field_names.values()))
+
+        field_values = {
+            param_python_name: self.object_to_convert[param_yaml_name]
+            for param_python_name, param_yaml_name in field_names.items()
+        }
+        return self.ScopeConditionDataclass(**field_values)
+
+    def _get_action_enum_from_string(self, action_str: str) -> ScopeConditionAction:
+        for action in ScopeConditionAction:
+            if action.value == action_str:
+                return action
+        raise ValueError(f"Invalid value for Action: '{action_str}'")
