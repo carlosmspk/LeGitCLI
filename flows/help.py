94f0e7aa3c7254e6c60937183681367afc32f7f3
carlosmspk <carlos_cp_santos@hotmail.com>
2023-11-02 21:38:06 +0000

    #21 Add docstring to help flow and capitalize printed message

--- flows/help.py
+++ flows/help.py
@@ -1,11 +1,12 @@
 def run_flow():
+    """This flow prints out legit usage and returns"""
     from flows import Flows
 
     valid_legit_verbs = "\n".join("\t- " + i for i in Flows.get_flow_names())
     print(
         "Command usage:",
         "\tlegit <verb> [args]",
-        "valid verbs:",
+        "Valid verbs:",
         valid_legit_verbs,
         sep="\n",
     )
