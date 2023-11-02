b0e223187c44eb485d61740c44bff8642410765d
carlosmspk <carlos_cp_santos@hotmail.com>
2023-11-02 19:12:42 +0000

    #18 Add new bindings

--- model/bindings.py
+++ model/bindings.py
@@ -1,2 +1,4 @@
 rule_bindings_map: dict[str, type] = {}
 scope_bindings_map: dict[str, type] = {}
+rule_validator_bindings_map: dict[type, type] = {}
+scope_matcher_bindings_map: dict[type, type] = {}
