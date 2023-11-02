73328f6dcdd87cdb5c0b99076093f1d36d0dc98d
carlosmspk <carlos_cp_santos@hotmail.com>
2023-11-02 21:29:40 +0000

    #3 Remove pointless git client params

--- git/client.py
+++ git/client.py
@@ -1,5 +1,5 @@
-from os import PathLike
-from model.command_configs import ErrorBehavior, OutputLevel
+from command import run_command
+import os
 
 
 class GitReadonlyClient:
@@ -7,14 +7,6 @@ class GitReadonlyClient:
     Client class that acts upon a git repository found in the process's current
     directory, but does not change the repository's state
     """
-
-    def __init__(
-        self,
-        output_level: OutputLevel = OutputLevel.STANDARD,
-        on_error: ErrorBehavior = ErrorBehavior.RAISE_EXCEPTION,
-    ) -> None:
-        self.__output_level: OutputLevel = output_level
-        self.__on_error: ErrorBehavior = on_error
 
     def get_current_branch(self) -> str:
         """Returns the current active branch's name"""
