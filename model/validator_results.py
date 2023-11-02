75ef32cd2c542489b53235ebcd47595a8c0a48e3
carlosmspk <carlos_cp_santos@hotmail.com>
2023-11-02 19:17:14 +0000

    #18 Update validator top level types

--- model/validator_results.py
+++ model/validator_results.py
@@ -1,16 +1,16 @@
 from dataclasses import dataclass
+from typing import Sequence
 from model.rules import Rule
 
 
-@dataclass(frozen=True)
+@dataclass
 class FailedRule:
     rule: Rule
-    container_scope_name: str
     failed_reason: str
 
 
-@dataclass(frozen=True)
-class CommitScopedRulesetVerifierResult:
+@dataclass
+class CommitScopedRulesetValidatorResult:
     triggered_scope: str | None
     should_fail: bool
-    failed_rules: list[FailedRule] = tuple()
+    failed_rules: Sequence[FailedRule] = tuple()
