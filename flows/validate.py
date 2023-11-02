efdd562b09a6a24cb955ebeb277333505647be6c
carlosmspk <carlos_cp_santos@hotmail.com>
2023-11-02 19:15:53 +0000

    #18 Update validate flow to use new validators

--- flows/validate.py
+++ flows/validate.py
@@ -1,34 +1,48 @@
-import os
 from git.client import GitReadonlyClient
 from parsing import parse_legit_file
-from verifiers.commit import CommitVerifier
+from utils import LazyFileReader
+from validator.commit import CommitValidator
 
-__LEGIT_FILE_PATH = "legitrules.yml"
-__DEV_MODE=True #TODO: define this as command arg
+__DEV_MODE = True  # TODO: define this as command arg
 
 
 def run_flow():
     """This flow validates an ongoing git process, such as a commit, and exits
     with exit code 0 if git process is valid, or non-zero otherwise"""
-    if not __DEV_MODE and __LEGIT_FILE_PATH not in os.listdir(os.curdir):
-        raise FileNotFoundError("Could not find required file 'legitrules.yml'")
-
-    dummy_commit_message_file = (
-        "dummy.txt"  # TODO: retrieve this as argument to legit CLI
+    legit_file = LazyFileReader(
+        "legitrules.yml" if not __DEV_MODE else "resources/rulefiles/samples/simple.yml"
     )
 
-    scoped_rulesets, config = parse_legit_file(__LEGIT_FILE_PATH)
+    commit_message_file = LazyFileReader(
+        "resources/message/short_message.txt"
+    )  # TODO: retrieve this as argument to legit CLI
 
-    verifier = CommitVerifier(GitReadonlyClient())
-    triggered_scopes_failing_results = verifier.verify_commit(scoped_rulesets, dummy_commit_message_file)
+    scoped_rulesets, config = parse_legit_file(legit_file)
+
+    validator = CommitValidator(GitReadonlyClient(), commit_message_file, config)
+    triggered_scopes_results = validator.validate_commit(scoped_rulesets)
+    failed_triggered_scopes_results = list(
+        filter(lambda x: x.should_fail, triggered_scopes_results)
+    )
 
     if __DEV_MODE:
-        for failing_scope_result in triggered_scopes_failing_results:
-            failed_rules_message ="\n\t- " + "\n\t- ".join(i.failed_reason for i in failing_scope_result.failed_rules)
-            print(f"Scope '{failing_scope_result.triggered_scope}' was triggered and failed because:{failed_rules_message}")
+        for failing_scope_result in triggered_scopes_results:
+            failed_rules_message = (
+                "and failed because:\n\t- "
+                + "\n\t- ".join(
+                    i.failed_reason for i in failing_scope_result.failed_rules
+                )
+                if failing_scope_result.should_fail
+                else "and was validated."
+            )
+            print(
+                f"Scope '{failing_scope_result.triggered_scope}' was triggered {failed_rules_message}"
+            )
+        if len(triggered_scopes_results) < 1:
+            print(f"No scopes were triggered. Nothing to do.")
 
     # TODO: display better info as to why commit failed
-    if len(triggered_scopes_failing_results) > 0:
+    if len(failed_triggered_scopes_results) > 0:
         exit(1)
 
 
