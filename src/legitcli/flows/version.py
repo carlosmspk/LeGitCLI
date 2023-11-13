019c71e8c45a4e5a155e0b4981c586017403d827
carlosmspk <carlosmspk@gmail.com>
2023-11-13 19:19:30 +0000

    LG-24 Update version flow to include project name

--- src/legitcli/flows/version.py
+++ src/legitcli/flows/version.py
@@ -3,8 +3,4 @@ from typing import List
 
 
 def run_flow(args: List[str]):
-    try:
-        print(importlib.metadata.version("legitcli"))
-    except importlib.metadata.PackageNotFoundError:
-        # Handle the case where the package is not installed
-        print("unknown")
+    print(f"LeGit CLI {importlib.metadata.version('legitcli')}")
